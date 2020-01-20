0d4abed18d7b0f3d593be3731c3847e3e0f56a41
TodoresteG <tgyuzelev+1@gmail.com>
2020-01-20 17:09:38 +0200

    ORM Fundamentals

---
+++ Entity Framework Core/2.ORM Fundamentals/MiniORM.App/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace MiniORM.App
+{
+    using MiniORM.App.Data;
+    using MiniORM.App.Data.Entities;
+    using System;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //I really suck on this one and it wasted me a lot of time and gave me headaches... HOOOORAAAAAY
+
+            string connectionString = @"Server=DESKTOP-NNVF465\SQLEXPRESS;Database=MiniORM;Integrated Security=True";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            Employee employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Entity Framework Core/2.ORM Fundamentals/MiniORM/ChangeTracker.cs
@@ -0,0 +1,145 @@
+﻿namespace MiniORM
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    internal class ChangeTracker<T>
+        where T : class, new()
+    {
+        private readonly List<T> allEntities;
+
+        private readonly List<T> added;
+
+        private readonly List<T> removed;
+
+        public ChangeTracker(IEnumerable<T> entities)
+        {
+            this.added = new List<T>();
+            this.removed = new List<T>();
+
+            this.allEntities = CloneEntities(entities);
+        }
+
+        public IReadOnlyCollection<T> AllEntities => this.allEntities.AsReadOnly();
+
+        public IReadOnlyCollection<T> Added => this.added.AsReadOnly();
+
+        public IReadOnlyCollection<T> Removed => this.removed.AsReadOnly();
+
+        public void Add(T item) => this.added.Add(item);
+
+        //public void Remove(T item) => this.removed.Add(item);
+        public void Remove(T entity) 
+        {
+            this.removed.Add(entity);
+
+            var entityToDelete = this.GetEntityToDelete(entity);
+
+            this.allEntities.Remove(entityToDelete);
+        }
+
+        public void ClearAdded() 
+        {
+            this.added.Clear();
+        }
+
+        public void ClearRemoved() 
+        {
+            this.removed.Clear();
+        }
+
+        public IEnumerable<T> GetModifiedEntities(DbSet<T> dbSet)
+        {
+            List<T> modifiedEntities = new List<T>();
+
+            var primaryKeys = typeof(T)
+                .GetProperties()
+                .Where(pi => pi.HasAttribute<KeyAttribute>())
+                .ToArray();
+
+            foreach (T proxyEntity in this.AllEntities)
+            {
+                var primaryKeyValues = GetPrimaryKeyValues(primaryKeys, proxyEntity)
+                    .ToArray();
+
+                var entity = dbSet.Entities
+                    .SingleOrDefault(e => GetPrimaryKeyValues(primaryKeys, e)
+                    .SequenceEqual(primaryKeyValues));
+
+                if (entity != null)
+                {
+                    var isModified = IsModified(proxyEntity, entity);
+
+                    if (isModified)
+                    {
+                        modifiedEntities.Add(entity);
+                    }
+                }
+            }
+
+            return modifiedEntities;
+        }
+
+        private static List<T> CloneEntities(IEnumerable<T> entities)
+        {
+            List<T> clonedEntities = new List<T>();
+
+            PropertyInfo[] propertiesToClone = typeof(T)
+                .GetProperties()
+                .Where(pi => DbContext.AllowedSqlTypes.Contains(pi.PropertyType))
+                .ToArray();
+
+
+            foreach (T entity in entities)
+            {
+                T clonedEntity = Activator.CreateInstance<T>();
+
+                foreach (PropertyInfo prop in propertiesToClone)
+                {
+                    object value = prop.GetValue(entity);
+                    prop.SetValue(clonedEntity, value);
+                }
+
+                clonedEntities.Add(clonedEntity);
+            }
+
+            return clonedEntities;
+        }
+
+        private static bool IsModified(T proxyEntity, T entity)
+        {
+            var monitoredProperties = typeof(T)
+                .GetProperties()
+                .Where(pi => DbContext.AllowedSqlTypes.Contains(pi.PropertyType));
+
+            PropertyInfo[] modifiedProperties = monitoredProperties
+                .Where(pi => !Equals(pi.GetValue(entity), pi.GetValue(proxyEntity)))
+                .ToArray();
+
+            return monitoredProperties.Any();
+        }
+
+        private static IEnumerable<object> GetPrimaryKeyValues(IEnumerable<PropertyInfo> primaryKeys, T entity)
+        {
+            return primaryKeys.Select(pi => pi.GetValue(entity));
+        }
+
+        private T GetEntityToDelete(T entity) 
+        {
+            var primaryKeys = typeof(T)
+                .GetProperties()
+                .Where(pi => pi.HasAttribute<KeyAttribute>())
+                .ToArray();
+
+            var entityPrimaryKeysValues = GetPrimaryKeyValues(primaryKeys, entity);
+
+            T entityToDelete = this.AllEntities
+                .Single(e => GetPrimaryKeyValues(primaryKeys, entity).SequenceEqual(entityPrimaryKeysValues));
+
+            return entityToDelete;
+        }
+    }
+}
