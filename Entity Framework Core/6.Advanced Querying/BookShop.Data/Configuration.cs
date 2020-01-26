baffd8a6e4fcb64f5dda5086ff2ff0ee4b294f6b
TodoresteG <tgyuzelev+1@gmail.com>
2020-01-26 16:50:02 +0200

    Advanced Querying

---
+++ Entity Framework Core/6.Advanced Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString => @"Server=DESKTOP-NNVF465\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
